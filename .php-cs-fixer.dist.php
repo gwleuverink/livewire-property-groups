7ac1e59348f198f3ecb6e9942cda4c5cb6c35b71
gwleuverink <willem@leuver.ink>
2024-08-25 23:39:02 +0200

    update package boilerplate

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,7 +8,7 @@ use PhpCsFixer\Finder;
 $finder = Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/config',
+        // __DIR__ . '/config',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,5 @@
+<?php
+
+it('returns true', function () {
+    exptect(true)->toBe(true);
+});
