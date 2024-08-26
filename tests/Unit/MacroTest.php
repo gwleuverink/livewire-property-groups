bb59b1df5c7815d3e13363dbd69137f4b1f93e79
gwleuverink <willem@leuver.ink>
2024-08-26 03:07:23 +0200

    add more failing tests

--- tests/Unit/MacroTest.php
+++ tests/Unit/MacroTest.php
@@ -63,6 +63,24 @@ it('has no unexpected side effects if both keys() and values() are called in the
         ]);
 });
 
+it('can iterate over properties in a group when the each() method was chained', function () {
+    $component = new class extends TestComponent
+    {
+        #[Group('a')]
+        public $foo = 1;
+
+        #[Group('a')]
+        public $bar = 2;
+    };
+
+    $iterations = 0;
+
+    group($component, 'a')->each(fn () => $iterations++);
+
+    expect($iterations)->toBe(2);
+});
+
 // Should be Integration tests? Component not mounted on unit level. Can't reset properties this way
 it('resets all properties in a group when the reset() method was chained')->todo(); // add two groups for unhappy path
 it('returns & resets all properties in a group when the pull() method was chained')->todo(); // add two groups for unhappy path
+it('validates all properties in a group when the validate() method was chained')->todo(); // add two groups for unhappy path
